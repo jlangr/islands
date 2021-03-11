cdb2236add6aff99ac3dc37f33143ce543bf5517
Jeff L <jeff@langrsoft.com>
2021-03-10 19:54:39 -0700

    book p89

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -17,6 +17,9 @@ defmodule IslandsEngine.Game do
   defp update_rules(state_data, rules), do:
     %{ state_data | rules: rules }
 
+  defp update_board(state_data, player, board), do:
+    Map.update!(state_data, player, fn player -> %{player | board: board} end)
+
   defp reply_success(state_data, reply), do:
     {:reply, reply, state_data}
 
@@ -32,11 +35,59 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:position_island, player, key, row, col}, _from, state_data) do
+    board = player_board(state_data, player)
+    with {:ok, rules} <-
+           Rules.check(state_data.rules, {:position_islands, player}),
+         {:ok, coordinate} <-
+           Coordinate.new(row, col),
+         {:ok, island} <-
+           Island.new(key, coordinate),
+         %{}=board <-
+           Board.position_island(board, key, island)
+    do
+      state_data
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error ->
+        {:reply, :error, state_data}
+      {:error, :invalid_coordinate} -> 
+        {:reply, {:error, :invalid_coordinate}, state_data}
+      {:error, :invalid_island_type} -> 
+        {:reply, {:error, :invalid_island_type}, state_data}
+    end
+  end
+
+  def handle_call({:set_islands, player}, _from, state_data) do
+    board = player_board(state_data, player)
+    with {:ok, rules} <- Rules.check(state_data.rules, {:set_islands, player}),
+         true         <- Board.all_islands_positioned?(board)
+    do
+      state_data
+      |> update_rules(rules)
+      |> reply_success({:ok, board})
+    else
+      :error -> {:reply, :error, state_data}
+      false  -> {:reply, {:error, :not_all_islands_positioned}, state_data}
+    end
+  end
+
+  defp player_board(state_data, player), do:
+    Map.get(state_data, player).board
+
   # game interface:
   
+  def start_link(name) when is_binary(name), do:
+    GenServer.start_link(__MODULE__, name, [])
+
   def add_player(game, name) when is_binary(name), do:
     GenServer.call(game, {:add_player, name})
 
-  def start_link(name) when is_binary(name), do:
-    GenServer.start_link(__MODULE__, name, [])
+  def position_island(game, player, key, row, col) when player in @players, do:
+    GenServer.call(game, {:position_island, player, key, row, col})
+
+  def set_islands(game, player) when player in @players, do:
+    GenServer.call(game, {:set_islands, player})
 end
